82923a255db35a81d388bfbfebc854d64344bbc5
AGagnidze <agagnidze@tbcbank.com.ge>
2022-01-03 15:49:13 +0400

    project kickoff

---
+++ app/src/main/java/com/example/moviesapp/ui/TvShowsFragment.kt
@@ -0,0 +1,13 @@
+package com.example.moviesapp.ui
+
+import com.example.moviesapp.base.BaseFragment
+import com.example.moviesapp.databinding.TvShowsFragmentBinding
+
+class TvShowsFragment : BaseFragment<TvShowsFragmentBinding>(TvShowsFragmentBinding::inflate) {
+
+    override fun setUpFragment() {
+        TODO("Not yet implemented")
+    }
+
+
+}
---
+++ app/src/main/java/com/example/moviesapp/ui/TvShowsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.moviesapp.ui
+
+import androidx.lifecycle.ViewModel
+
+class TvShowsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
