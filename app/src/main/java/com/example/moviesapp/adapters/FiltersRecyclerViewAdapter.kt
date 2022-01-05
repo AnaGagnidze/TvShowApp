9d127db1f16f48446188cc6aa0dffa76b4525828
AGagnidze <agagnidze@tbcbank.com.ge>
2022-01-05 13:16:54 +0400

    added filter selection feature

--- app/src/main/java/com/example/moviesapp/adapters/FiltersRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/moviesapp/adapters/FiltersRecyclerViewAdapter.kt
@@ -1,21 +1,57 @@
 package com.example.moviesapp.adapters
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.example.moviesapp.R
 import com.example.moviesapp.databinding.ItemFilterLayoutBinding
 
-class FiltersRecyclerViewAdapter(private val items: MutableList<String>):RecyclerView.Adapter<FiltersRecyclerViewAdapter.FiltersViewHolder>() {
+typealias OnFilterClick = (position: Int) -> Unit
+
+class FiltersRecyclerViewAdapter(private val items: MutableList<String>) :
+    RecyclerView.Adapter<FiltersRecyclerViewAdapter.FiltersViewHolder>() {
 
 
-    inner class FiltersViewHolder(private val binding: ItemFilterLayoutBinding): RecyclerView.ViewHolder(binding.root) {
-        fun bind(){
+    private var currentFilterPosition: Int = 0
+    lateinit var onDrawableClick: OnFilterClick
+
+    inner class FiltersViewHolder(private val binding: ItemFilterLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root), View.OnClickListener {
+        fun bind() {
             binding.filterTxt.text = items[adapterPosition]
+            binding.root.setOnClickListener(this)
+
+            if (currentFilterPosition == adapterPosition) {
+                binding.rootLayout.isSelected = true
+                binding.filterTxt.setTextColor(
+                    ContextCompat.getColor(
+                        binding.root.context,
+                        R.color.white
+                    )
+                )
+            }else{
+                binding.rootLayout.isSelected = false
+                binding.filterTxt.setTextColor(
+                    ContextCompat.getColor(
+                        binding.root.context,
+                        R.color.filter_txt_clr
+                    )
+                )
+            }
+        }
+
+        override fun onClick(p0: View?) {
+            currentFilterPosition = adapterPosition
+            notifyDataSetChanged()
+//            onDrawableClick.invoke(adapterPosition)
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = FiltersViewHolder(
-        ItemFilterLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+        ItemFilterLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+    )
 
     override fun onBindViewHolder(holder: FiltersViewHolder, position: Int) {
         holder.bind()
