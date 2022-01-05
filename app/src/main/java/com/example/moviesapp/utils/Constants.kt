1b59be48ebc2ffea350b03db48dfbb4f594ea9e7
AGagnidze <agagnidze@tbcbank.com.ge>
2022-01-05 13:15:59 +0400

    added new dependencies and constants

---
+++ app/src/main/java/com/example/moviesapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.moviesapp.utils
+
+object Constants {
+    const val SHOWS_BASE_URL = "https://api.themoviedb.org/3/tv/"
+    const val API_KEY = "3e79f482077c20ac41f6785800f8b1cc"
+
+    const val IMAGE_URL_W500 = "https://image.tmdb.org/t/p/w500/"
+}
