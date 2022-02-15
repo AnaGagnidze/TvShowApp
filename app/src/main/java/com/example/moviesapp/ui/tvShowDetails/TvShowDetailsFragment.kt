67cf1ab796f998314c31ee419399a0d3bd3e728c
AGagnidze <agagnidze@tbcbank.com.ge>
2022-02-15 15:39:33 +0400

    set up for show details fragment

---
+++ app/src/main/java/com/example/moviesapp/ui/tvShowDetails/TvShowDetailsFragment.kt
@@ -0,0 +1,11 @@
+package com.example.moviesapp.ui.tvShowDetails
+
+import com.example.moviesapp.base.BaseFragment
+import com.example.moviesapp.databinding.TvShowDetailsFragmentBinding
+
+class TvShowDetailsFragment: BaseFragment<TvShowDetailsFragmentBinding>(TvShowDetailsFragmentBinding::inflate) {
+
+    override fun setUpFragment() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/example/moviesapp/ui/tvShowDetails/TvShowDetailsViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.moviesapp.ui.tvShowDetails
+
+import androidx.lifecycle.ViewModel
+
+class TvShowDetailsViewModel: ViewModel() {
+}
