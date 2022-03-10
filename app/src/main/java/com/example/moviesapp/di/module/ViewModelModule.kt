4cf49c6a7d1e14fc3d3c3dfa55d7ecc66fd62897
AGagnidze <agagnidze@tbcbank.com.ge>
2022-03-10 14:49:18 +0400

    added another view model

--- app/src/main/java/com/example/moviesapp/di/module/ViewModelModule.kt
+++ app/src/main/java/com/example/moviesapp/di/module/ViewModelModule.kt
@@ -1,9 +1,12 @@
 package com.example.moviesapp.di.module
 
+import com.example.moviesapp.ui.tvShowDetails.TvShowDetailsViewModel
 import com.example.moviesapp.ui.tvShows.TvShowsViewModel
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModel { TvShowsViewModel(get()) }
+    viewModel { (tvId: String?) ->
+        TvShowDetailsViewModel(tvId, get()) }
 }
--- app/src/main/java/com/example/moviesapp/ui/tvShowDetails/TvShowDetailsViewModel.kt
+++ app/src/main/java/com/example/moviesapp/ui/tvShowDetails/TvShowDetailsViewModel.kt
@@ -1,6 +1,53 @@
 package com.example.moviesapp.ui.tvShowDetails
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.*
+import com.example.moviesapp.adapters.tvShowsAdapter.TvShowsDataSource
+import com.example.moviesapp.models.showDetailModels.ShowDetailResponseModel
+import com.example.moviesapp.models.showsModels.ShowsResultModel
+import com.example.moviesapp.network.ResultControl
+import com.example.moviesapp.repository.TvShowsRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class TvShowDetailsViewModel: ViewModel() {
+class TvShowDetailsViewModel(private val tvId: String?,
+                             private val tvShowsRepository: TvShowsRepository): ViewModel() {
+
+    private val showDetails = MutableLiveData<ResultControl<ShowDetailResponseModel?>>().apply {
+        mutableListOf<ShowDetailResponseModel>()
+    }
+
+    val _showDetails: LiveData<ResultControl<ShowDetailResponseModel?>> = showDetails
+
+
+    init {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                getShowDetails(tvId?.toInt())
+            }
+        }
+    }
+
+    private suspend fun getShowDetails(tvId: Int?) {
+        val result = tvShowsRepository.getShowDetails(tvId)
+        if (result.isSuccessful) {
+            val info = result.body()
+            info.let {
+                showDetails.postValue(ResultControl.success(it))
+            }
+        }else {
+            showDetails.postValue(ResultControl.error(result.errorBody().toString()))
+        }
+    }
+
+    fun getSimilarShows(): LiveData<PagingData<ShowsResultModel>> {
+        return Pager(PagingConfig(pageSize = 1)) {
+            TvShowsDataSource(tvShowsRepository = tvShowsRepository, tvId = tvId?.toInt())
+        }.liveData.cachedIn(viewModelScope)
+    }
 }
+
